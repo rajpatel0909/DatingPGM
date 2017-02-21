c084cd8fc3cba76cd6d2ec959d920f332968cf00
Mihir2 <mihirchauhan9@gmail.com>
2017-02-21 11:08:41 -0500

    Pushing TP

--- buildPGM.py
+++ buildPGM.py
@@ -18,22 +18,22 @@ from pgmpy.estimators import MaximumLikelihoodEstimator
 from pgmpy.estimators import ParameterEstimator
 from pgmpy.inference import VariableElimination
 
-if __name__ == "__main__":
-    print "Hello World"
-    
-    #df = pd.read_csv("C:/MyStuff/SEM2/AML/Project1/tes.csv");
-    df = pd.read_csv("SpeedDating_discrete02.csv");
-    file1 = open("cmd.txt");
-    file2 = file.read(file1);
-    model = eval(file2)
-    #Finding CPDS
-    pe = ParameterEstimator(model, df) 
-     #CPD FOR NODE: LIKE
-    cpd = pe.state_counts('match')
-    cpd = cpd.transpose() #Helps when summing
-    #print cpd
-    #Baap Code to get the probabilities from the frquencies.
-    cpd_prob = cpd.div(cpd.sum(axis=1), axis=0) 
-    #Remove the divide b 0 error
-    cpd_prob = cpd_prob.fillna(0)
-    print cpd_prob
+
+print "Hello World"
+
+#df = pd.read_csv("C:/MyStuff/SEM2/AML/Project1/tes.csv");
+df = pd.read_csv("SpeedDating_discrete02.csv");
+file1 = open("cmd.txt");
+file2 = file.read(file1);
+model = eval(file2)
+#Finding CPDS
+pe = ParameterEstimator(model, df) 
+ #CPD FOR NODE: LIKE
+cpd = pe.state_counts('match')
+cpd = cpd.transpose() #Helps when summing
+#print cpd
+#Baap Code to get the probabilities from the frquencies.
+cpd_prob = cpd.div(cpd.sum(axis=1), axis=0) 
+#Remove the divide b 0 error
+cpd_prob = cpd_prob.fillna(0)
+print cpd_prob
--- main.py
+++ main.py
@@ -1,1 +1,2 @@
+import makeLinks
 import buildPGM
