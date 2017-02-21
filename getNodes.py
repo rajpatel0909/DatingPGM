9935646e4bce183d24a62f1cbb90d61a8d1d6989
rajpatel0909 <rajp.uf16@gmail.com>
2017-02-21 13:34:36 -0500

    getNodes file added

--- getNodes.py
+++ getNodes.py
@@ -1,2 +1,7 @@
 def getNodesFromCSV():
-    
+    print("hello")
+    file = open("SpeedDating_discrete02.csv");
+    print(type(file))
+    for line in file.readlines():
+        array = line.split(',')
+        first_item = array[0]
